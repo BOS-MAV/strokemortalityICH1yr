d198dd9c19b62860372d970bd0a7cba29b689ea9
BOS-MAV <52087638+BOS-MAV@users.noreply.github.com>
2025-06-13 14:09:36 -0400

    bug fixes, minor changes

--- loadAvgArray_dynamicICH1yr.js
+++ loadAvgArray_dynamicICH1yr.js
@@ -1,22 +1,21 @@
-
 const measureEnum = {
-  AVGSYS : 0,
-  AVGDIA : 1,
-  AVGBMI : 2,
-  AVGCREAT : 3,
-  AVGCHOL : 4
+  AVGSYS: 0,
+  AVGDIA: 1,
+  AVGBMI: 2,
+  AVGCREAT: 3,
+  AVGCHOL: 4,
 };
 
 let avgLabs = {};
 
 function loadAvgArray() {
-  return fetch('avgLabsICH1yr.json')
-    .then(response => response.json())
-    .then(data => {
+  return fetch("avgLabsICH1yr.json")
+    .then((response) => response.json())
+    .then((data) => {
       avgLabs = data;
       console.log("Lab averages loaded", avgLabs);
     })
-    .catch(error => {
+    .catch((error) => {
       console.error("Failed to load average lab values:", error);
     });
 }
